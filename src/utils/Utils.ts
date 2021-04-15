c8d3452e2004dc6cf65b428d14aec8a340328135
Paul.Benn-Darias <paul.benn-darias@bjss.com>
2021-04-15 09:37:37 +0100

    Add debug logging

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -6,10 +6,14 @@ import { ERROR } from '../models/enums';
 import { Target, TargetConfig } from '../models/interfaces';
 
 export const getTargetFromSourceARN = (arn: string): Target => {
+  console.log("Source ARN", arn);
   const targets: TargetConfig = Configuration.getInstance().getTargets();
+  console.log("Possible targets", targets);
   const validTargets = Object.keys(targets).filter((k) => arn.includes(k));
+  console.log("Valid targets", targets);
   if (validTargets.length !== 1) {
     throw new Error(ERROR.NO_UNIQUE_TARGET);
   }
+  console.log("Found valid target", targets[validTargets[0]]);
   return targets[validTargets[0]];
 };
